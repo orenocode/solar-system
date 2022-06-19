5d1483bb6941265be3b06cac874fb65dc18c3fa1
Bảo <monkeyd.lucy2110@gmail.com>
2022-06-19 20:23:29 +0700

    update image source

--- planets/moon.js
+++ planets/moon.js
@@ -1,8 +1,8 @@
 import * as three from "three"
 import { camera, renderer, scene } from "../main"
 
-const moonTexture = new three.TextureLoader().load("textures/moon_texture.jpg")
-const surfaceTexture = new three.TextureLoader().load("textures/moon_surface_texture.jpg")
+const moonTexture = new three.TextureLoader().load("public/moon_texture.jpg")
+const surfaceTexture = new three.TextureLoader().load("public/moon_surface_texture.jpg")
 
 export const moon = new three.Mesh(
     new three.SphereGeometry(2, 200, 200),
